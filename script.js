f4cf5100225058b4e69ce184ec666742aedd8284
Viktor Sharha <vsharha07@gmail.com>
2024-08-16 18:02:23 +0100

    formatting

--- script.js
+++ script.js
@@ -9,133 +9,135 @@ const buttons = document.querySelectorAll("button");
 // on ready
 
 function onReady() {
-    update_screen(0);
+	update_screen(0);
 
-    for(let button of buttons) {
-        button.addEventListener("click", function(){process_input(this)});
-    }
+	for (let button of buttons) {
+		button.addEventListener("click", function () {
+			process_input(this);
+		});
+	}
 }
 
-if(document.readyState !== "loading") {
-    onReady();
+if (document.readyState !== "loading") {
+	onReady();
 } else {
-    document.addEventListener("DOMContentLoaded", onReady());
+	document.addEventListener("DOMContentLoaded", onReady());
 }
 
 // screen
 
-function update_screen(value=display) {
-    if(value == "") {
-        value = "0";
-    }
+function update_screen(value = display) {
+	if (value == "") {
+		value = "0";
+	}
 
-    display = String(value);
-    
-    document.querySelector("#calculator #screen").innerHTML = display;
+	display = String(value);
+
+	document.querySelector("#calculator #screen").innerHTML = display;
 }
 
 function update_calculation(value) {
-    previous_calculation = calculation;
-    calculation = value;
-    update_screen(calculation);
+	previous_calculation = calculation;
+	calculation = value;
+	update_screen(calculation);
 }
 
 function save_display_val() {
-    let number = Number(display);
+	let number = Number(display);
 
-    if(number) {
-        update_calculation(number);
-        update_screen(0);
-    } else {
-        update_screen(NaN);
-    }
+	if (number) {
+		update_calculation(number);
+		update_screen(0);
+	} else {
+		update_screen(NaN);
+	}
 
-    return number;
+	return number;
 }
 
 // functions
 
 function delete_char() {
-    display = display.slice(0,-1);
-    update_screen();
+	display = display.slice(0, -1);
+	update_screen();
 }
 
 function change_sign() {
-    let number = Number(display);
+	let number = Number(display);
 
-    if(number) {
-        calculation = -number;
-        update_screen(calculation);
-    }
+	if (number) {
+		calculation = -number;
+		update_screen(calculation);
+	}
 }
 
 // buttons
 
 function process_input(el) {
-    let value = el.innerHTML;
+	let value = el.innerHTML;
 
-    switch(el.className) {
-        case "number":
-            handle_number_in(value);
-            break;
-        case "function":
-            handle_func(value);
-            break;
-        case "operation":
-            handle_operations(value);
-            break;
-        case "equal":
-            handle_equal();
-            break;
-    }
+	switch (el.className) {
+		case "number":
+			handle_number_in(value);
+			break;
+		case "function":
+			handle_func(value);
+			break;
+		case "operation":
+			handle_operations(value);
+			break;
+		case "equal":
+			handle_equal();
+			break;
+	}
 }
 
 function handle_number_in(number) {
-    if(number == "." && display.includes(".")) {
-        return;
-    }
+	if (number == "." && display.includes(".")) {
+		return;
+	}
 
-    if(display == "0" && number != ".") {
-        display = "";
-    }
+	if (display == "0" && number != ".") {
+		display = "";
+	}
 
-    display += number;
+	display += number;
 
-    update_screen();
+	update_screen();
 }
 
 function handle_func(func) {
-    switch(func) {
-        case "AC":
-            update_calculation(0);
-            break;
-        case "DEL":
-            delete_char();
-            break;
-        case "±":
-            change_sign();
-            break;
-    }
+	switch (func) {
+		case "AC":
+			update_calculation(0);
+			break;
+		case "DEL":
+			delete_char();
+			break;
+		case "±":
+			change_sign();
+			break;
+	}
 }
 
 function handle_operations(operation) {
-    if(display == "0") {
-        if(operation == "-") {
-            handle_number_in(operation);
-        }
+	if (display == "0") {
+		if (operation == "-") {
+			handle_number_in(operation);
+		}
 
-        return;
-    }
+		return;
+	}
 
-    if(save_display_val()) {
-        last_op = operation;
-    }
+	if (save_display_val()) {
+		last_op = operation;
+	}
 }
 
 function handle_equal() {
-    if(last_op && last_op !== "=" && save_display_val()) {
-        let result = eval(previous_calculation + last_op + calculation);
-        last_op = "=";
-        update_calculation(result);
-    }    
+	if (last_op && last_op !== "=" && save_display_val()) {
+		let result = eval(previous_calculation + last_op + calculation);
+		last_op = "=";
+		update_calculation(result);
+	}
 }
